118e91cc965269f026d3a805f254cdb5b648d23f
qianyue92 <qianyue@shein.com>
2018-07-27 15:59:06 +0800

    效率测试

--- src/main/java/test/TestLambda1.java
+++ src/main/java/test/TestLambda1.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class TestLambda1 {
@@ -88,4 +89,177 @@ public class TestLambda1 {
 
         pirates.stream().filter((e) -> e.getReward() < 20000).forEach(System.out::println);
     }
+
+
+    // 效率测试
+    @Test
+    public void test5() {
+        for( int t=1;t<5;t++){
+            for(int tt=1;tt<=4;tt++){
+
+                List<Integer> testList = Arrays.asList(new Integer[(int) Math.pow(100,(t))]);
+                long t1 = System.currentTimeMillis();
+                //1.
+                for(int i=0;i<testList.size();i++){
+                    Integer b = testList.get(i);
+                }
+                long t2 = System.currentTimeMillis();
+                //2.
+                for(Integer i:testList){
+                    Integer b = i;
+                }
+                long t3 = System.currentTimeMillis();
+                //3.
+                testList.forEach(integer -> {Integer b = integer;});
+                long t4 = System.currentTimeMillis();
+                //4.
+                testList.stream().forEach(integer -> {Integer b = integer;});
+                long t5 = System.currentTimeMillis();
+                //5.
+                testList.parallelStream().forEach(integer -> {Integer b = integer;});
+                long t6 = System.currentTimeMillis();
+                Integer b;
+                //6.
+                for(Iterator<Integer> iterator = testList.iterator(); iterator.hasNext(); b = iterator.next());
+                long t7 = System.currentTimeMillis();
+                System.out.println("x" + tt +": loop size:" + testList.size());
+                System.out.println("y" + t + ": classical for loop waste millisecond:"+(t2-t1));
+                System.out.println("y" + t + ": classical forEach loop waste millisecond:"+(t3-t2));
+                System.out.println("y" + t + ": lambda forEach loop waste millisecond:"+(t4-t3));
+                System.out.println("y" + t + ": lambda not parallel stream forEach loop waste millisecond:"+(t5-t4));
+                System.out.println("y" + t + ": lambda parallel stream forEach loop waste millisecond:"+(t6-t5));
+                System.out.println("y" + t + ": classical iterator loop waste millisecond:"+(t7-t6)+"\n");
+            }
+        }
+    }
+
+
+    /*x1: loop size:100
+    y1: classical for loop waste millisecond:0
+    y1: classical forEach loop waste millisecond:0
+    y1: lambda forEach loop waste millisecond:46
+    y1: lambda not parallel stream forEach loop waste millisecond:2
+    y1: lambda parallel stream forEach loop waste millisecond:4
+    y1: classical iterator loop waste millisecond:0
+
+    x2: loop size:100
+    y1: classical for loop waste millisecond:0
+    y1: classical forEach loop waste millisecond:0
+    y1: lambda forEach loop waste millisecond:0
+    y1: lambda not parallel stream forEach loop waste millisecond:0
+    y1: lambda parallel stream forEach loop waste millisecond:0
+    y1: classical iterator loop waste millisecond:0
+
+    x3: loop size:100
+    y1: classical for loop waste millisecond:0
+    y1: classical forEach loop waste millisecond:0
+    y1: lambda forEach loop waste millisecond:0
+    y1: lambda not parallel stream forEach loop waste millisecond:0
+    y1: lambda parallel stream forEach loop waste millisecond:0
+    y1: classical iterator loop waste millisecond:0
+
+    x4: loop size:100
+    y1: classical for loop waste millisecond:0
+    y1: classical forEach loop waste millisecond:0
+    y1: lambda forEach loop waste millisecond:0
+    y1: lambda not parallel stream forEach loop waste millisecond:0
+    y1: lambda parallel stream forEach loop waste millisecond:0
+    y1: classical iterator loop waste millisecond:0
+
+    x1: loop size:10000
+    y2: classical for loop waste millisecond:1
+    y2: classical forEach loop waste millisecond:1
+    y2: lambda forEach loop waste millisecond:0
+    y2: lambda not parallel stream forEach loop waste millisecond:0
+    y2: lambda parallel stream forEach loop waste millisecond:1
+    y2: classical iterator loop waste millisecond:0
+
+    x2: loop size:10000
+    y2: classical for loop waste millisecond:1
+    y2: classical forEach loop waste millisecond:0
+    y2: lambda forEach loop waste millisecond:1
+    y2: lambda not parallel stream forEach loop waste millisecond:0
+    y2: lambda parallel stream forEach loop waste millisecond:1
+    y2: classical iterator loop waste millisecond:0
+
+    x3: loop size:10000
+    y2: classical for loop waste millisecond:1
+    y2: classical forEach loop waste millisecond:0
+    y2: lambda forEach loop waste millisecond:0
+    y2: lambda not parallel stream forEach loop waste millisecond:1
+    y2: lambda parallel stream forEach loop waste millisecond:0
+    y2: classical iterator loop waste millisecond:0
+
+    x4: loop size:10000
+    y2: classical for loop waste millisecond:1
+    y2: classical forEach loop waste millisecond:0
+    y2: lambda forEach loop waste millisecond:1
+    y2: lambda not parallel stream forEach loop waste millisecond:0
+    y2: lambda parallel stream forEach loop waste millisecond:0
+    y2: classical iterator loop waste millisecond:0
+
+    x1: loop size:1000000
+    y3: classical for loop waste millisecond:4
+    y3: classical forEach loop waste millisecond:7
+    y3: lambda forEach loop waste millisecond:5
+    y3: lambda not parallel stream forEach loop waste millisecond:4
+    y3: lambda parallel stream forEach loop waste millisecond:2
+    y3: classical iterator loop waste millisecond:12
+
+    x2: loop size:1000000
+    y3: classical for loop waste millisecond:10
+    y3: classical forEach loop waste millisecond:12
+    y3: lambda forEach loop waste millisecond:4
+    y3: lambda not parallel stream forEach loop waste millisecond:1
+    y3: lambda parallel stream forEach loop waste millisecond:0
+    y3: classical iterator loop waste millisecond:13
+
+    x3: loop size:1000000
+    y3: classical for loop waste millisecond:11
+    y3: classical forEach loop waste millisecond:15
+    y3: lambda forEach loop waste millisecond:1
+    y3: lambda not parallel stream forEach loop waste millisecond:2
+    y3: lambda parallel stream forEach loop waste millisecond:1
+    y3: classical iterator loop waste millisecond:17
+
+    x4: loop size:1000000
+    y3: classical for loop waste millisecond:10
+    y3: classical forEach loop waste millisecond:13
+    y3: lambda forEach loop waste millisecond:1
+    y3: lambda not parallel stream forEach loop waste millisecond:0
+    y3: lambda parallel stream forEach loop waste millisecond:1
+    y3: classical iterator loop waste millisecond:12
+
+    x1: loop size:100000000
+    y4: classical for loop waste millisecond:297
+    y4: classical forEach loop waste millisecond:117
+    y4: lambda forEach loop waste millisecond:62
+    y4: lambda not parallel stream forEach loop waste millisecond:56
+    y4: lambda parallel stream forEach loop waste millisecond:62
+    y4: classical iterator loop waste millisecond:98
+
+    x2: loop size:100000000
+    y4: classical for loop waste millisecond:125
+    y4: classical forEach loop waste millisecond:70
+    y4: lambda forEach loop waste millisecond:38
+    y4: lambda not parallel stream forEach loop waste millisecond:40
+    y4: lambda parallel stream forEach loop waste millisecond:34
+    y4: classical iterator loop waste millisecond:67
+
+    x3: loop size:100000000
+    y4: classical for loop waste millisecond:94
+    y4: classical forEach loop waste millisecond:73
+    y4: lambda forEach loop waste millisecond:45
+    y4: lambda not parallel stream forEach loop waste millisecond:41
+    y4: lambda parallel stream forEach loop waste millisecond:36
+    y4: classical iterator loop waste millisecond:69
+
+    x4: loop size:100000000
+    y4: classical for loop waste millisecond:97
+    y4: classical forEach loop waste millisecond:69
+    y4: lambda forEach loop waste millisecond:40
+    y4: lambda not parallel stream forEach loop waste millisecond:40
+    y4: lambda parallel stream forEach loop waste millisecond:34
+    y4: classical iterator loop waste millisecond:68*/
+
 }
--- src/main/java/test/TestStreamAPI.java
+++ src/main/java/test/TestStreamAPI.java
@@ -1,8 +1,10 @@
 package test;
 
 import dao.Pirate;
+import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
